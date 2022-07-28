aedd69bf96d3fffe67847a48ec317874cd86be38
millanadhikari <millanadhikari@gmail.com>
2022-07-28 15:13:44 +1000

    Anlytics component donee2

--- components/Admin/Home/Analytics/Analytics.tsx
+++ components/Admin/Home/Analytics/Analytics.tsx
@@ -1,26 +1,71 @@
-import { AddIcon } from '@chakra-ui/icons'
-import { Box, Flex, Text } from '@chakra-ui/react'
-import React from 'react'
-import VisitorChart from './VisitorChart'
+import { AddIcon } from "@chakra-ui/icons";
+import { Box, Flex, Text } from "@chakra-ui/react";
+import React from "react";
+import SpendChart from "./SpendChart";
+import VisitorChart from "./VisitorChart";
 
 export function Analytics() {
   return (
     <Box px={4} py={4}>
-        <Text fontSize="15px" color="gray.500" letterSpacing="0.7px" fontWeight="semibold">Analytics</Text>
-        <Flex bg="white" py={4} px={4} my={4}  rounded="md" flexDirection="column" h="180px" w="200px">
-            <Flex flexDirection="column" gap={1}>
-                <Text fontSize="sm" color="gray.500">Visitor this week</Text>
-                <Text fontWeight="semibold">80</Text>
-                <Flex color="red.500" alignItems="center" gap={2}><AddIcon fontSize="8px"/>10</Flex>
-            </Flex >
-            <Flex w="100%">
-            <VisitorChart/>
-            <Box w="50%"> </Box>
+      <Text
+        fontSize="15px"
+        color="gray.500"
+        letterSpacing="0.7px"
+        fontWeight="semibold"
+      >
+        Analytics
+      </Text>
+      <Flex gap={4}>
+        <Flex
+          bg="white"
+          py={4}
+          px={4}
+          my={4}
+          rounded="md"
+          flexDirection="column"
+          h="180px"
+        >
+          <Flex flexDirection="column" gap={1}>
+            <Text fontSize="sm" color="gray.500">
+              Visitor this week
+            </Text>
+            <Text fontWeight="semibold">80</Text>
+            <Flex color="red.500" alignItems="center" gap={2}>
+              <AddIcon fontSize="8px" />
+              10
             </Flex>
-            
+          </Flex>
+          <Flex w="100%">
+            <VisitorChart />
+          </Flex>
         </Flex>
-         </Box>
-  )
-  }
-  
-
+        <Flex
+          bg="white"
+          py={4}
+          px={4}
+          my={4}
+          rounded="md"
+          flexDirection="column"
+          h="180px"
+          w="170px"
+          
+        >
+          <Flex flexDirection="column" gap={1}>
+            <Text fontSize="sm" color="gray.500">
+              Spent this week
+            </Text>
+            <Text fontWeight="semibold">$80 / $300</Text>
+            <Flex color="gray.500" fontSize="13px" alignItems="center" gap={2}>
+              <AddIcon fontSize="8px" />
+              12 days left
+            </Flex>
+          </Flex>
+          <Flex w="100%" size="md" justifyContent="center" alignItems="center" mt="8">
+            <SpendChart />
+            
+          </Flex>
+        </Flex>
+      </Flex>
+    </Box>
+  );
+}
---
+++ components/Admin/Home/Analytics/SpendChart.tsx
@@ -0,0 +1,78 @@
+import React from "react";
+import { Bar } from "react-chartjs-2";
+
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+  } from 'chart.js';
+import { Box, Progress } from "@chakra-ui/react";
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+  );
+const labels =  ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul']
+const data = {
+    labels: labels,
+    datasets: [{
+      data: [65, 59, 80, 81, 56, 55, 40],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+        'rgba(255, 205, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(201, 203, 207, 0.2)'
+      ],
+      borderColor: [
+        'rgb(255, 99, 132)',
+        'rgb(255, 159, 64)',
+        'rgb(255, 205, 86)',
+        'rgb(75, 192, 192)',
+        'rgb(54, 162, 235)',
+        'rgb(153, 102, 255)',
+        'rgb(201, 203, 207)'
+      ],
+      borderWidth: 1,
+      barThickness: 3,
+      
+      
+    }]
+  };
+  const options = {
+    maintainAspectRatio: false,
+    plugins: {
+        legend: {
+          display: false
+        }
+      }, 
+      scales: {
+        y: {
+            display: false // Hide Y axis labels
+        },
+        x: {
+            display: false // Hide X axis labels
+        }
+    }   
+   
+}
+function SpendChart() {
+  return (
+    <Box w={40} h={16}>
+     <Progress value={20} size='xs' colorScheme='pink' />
+      
+    </Box>
+  );
+}
+
+export default SpendChart;
