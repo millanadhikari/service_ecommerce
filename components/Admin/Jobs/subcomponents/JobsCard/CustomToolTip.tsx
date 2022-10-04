a1fb06805d483ba2cc86203f21f31a6b545fe827
millanadhikari <millanadhikari@gmail.com>
2022-10-04 20:46:09 +1100

    lksjdddfdff

--- components/Admin/Jobs/subcomponents/JobsCard/CustomToolTip.tsx
+++ components/Admin/Jobs/subcomponents/JobsCard/CustomToolTip.tsx
@@ -2,27 +2,28 @@ import { Box, Icon, Tag, Tooltip } from "@chakra-ui/react";
 import React from "react";
 import { BiHelpCircle } from "react-icons/bi";
 
-const CustomCard = React.forwardRef(({ children, ...rest }, ref) => (
-  <Box p="1">
-    <Tag ref={ref} {...rest}>
-      {children}
-    </Tag>
-  </Box>
-));
+// const CustomCard = React.forwardRef(({ children, ...rest }, ref) => (
+//   <Box p="1">
+//     <Tag ref={ref} {...rest}>
+//       {children}
+//     </Tag>
+//   </Box>
+// ));
 
 const CustomToolTip = () => (
-  <Tooltip label="Total number of jobs completed and paid year-to-date">
-    <CustomCard>
-      <span>
-        <Icon
-          color="gray.500"
-          cursor="pointer"
-          as={BiHelpCircle}
-          _hover={{ color: "gray.700", fontSize: "17px" }}
-        />
-      </span>
-    </CustomCard>
-  </Tooltip>
+  <div>lkjsdf</div>
+  // <Tooltip label="Total number of jobs completed and paid year-to-date">
+  //   <CustomCard>
+  //     <span>
+  //       <Icon
+  //         color="gray.500"
+  //         cursor="pointer"
+  //         as={BiHelpCircle}
+  //         _hover={{ color: "gray.700", fontSize: "17px" }}
+  //       />
+  //     </span>
+  //   </CustomCard>
+  // </Tooltip>
 );
 
 export default CustomToolTip;
